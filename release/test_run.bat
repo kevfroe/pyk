553716b2a8d0f43a58384bd1687919500f9d5ba6
Kevin Rowe <krowe@litepoint.internal>
2019-03-13 10:49:59 -0700

    Adds newly built executable and improvements to batch file

--- release/test_run.bat
+++ release/test_run.bat
@@ -1,1 +1,9 @@
-remove_empty_rows.exe --file-in test_file.xlsx --file-out test_file_out.xlsx
+@echo off
+
+echo remove_empty_rows.exe --file-in ..\test\test_file.xlsx --file-out ..\test\test_file_out.xlsx
+remove_empty_rows.exe --file-in ..\test\test_file.xlsx --file-out ..\test\test_file_out.xlsx
+
+echo remove_empty_rows.exe --file-in ..\test\test_file.xlsx --file-out ..\test\test_file_out.xlsx
+remove_empty_rows.exe --file-in ..\test\test_file.xlsx --file-out ..\test\test_file_out.xlsx
+
+pause
